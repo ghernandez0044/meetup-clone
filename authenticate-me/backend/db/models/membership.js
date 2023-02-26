df610e7acebedb23a183608b8c7bdb8264082df1
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-25 19:28:38 -0800

    created Memberships table database schema, also migrated

---
+++ authenticate-me/backend/db/models/membership.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Membership extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Membership.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    groupId: {
+      type: DataTypes.INTEGER
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Membership',
+  });
+  return Membership;
+};
