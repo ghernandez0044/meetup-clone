52c5dacc83ad076f5fa0ecab4596f12eaf1bbe32
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-26 14:09:26 -0800

    progress check

---
+++ authenticate-me/backend/db/seeders/20230226055813-create-venues.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {}
+if(process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+options.tableName = 'Venues'
+
+const venues = [
+  {
+    groupId: 1,
+    address: '123 Street Name',
+    city: 'San Francisco',
+    state: 'California',
+    lat: 23.2342,
+    lng: 123.2313
+  }
+]
+
+
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
