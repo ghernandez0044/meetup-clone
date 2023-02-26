48090b792a7fe8ad2b3c0e4e8861dc7382f8222c
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-25 18:27:38 -0800

    Created Groups table schema and migrated it

---
+++ authenticate-me/backend/db/migrations/20230226021750-create-group.js
@@ -0,0 +1,67 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {}
+if(process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+options.tableName = 'Groups'
+
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      organizerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      name: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      },
+      about: {
+        type: Sequelize.STRING(254),
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(options);
+  }
+};
