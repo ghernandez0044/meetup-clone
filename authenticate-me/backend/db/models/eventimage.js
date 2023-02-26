7c2ddf5507d9ffe6d6a844a4d55fa8016824fcd3
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-25 19:43:18 -0800

    created EventImages table database schema and migrated it

---
+++ authenticate-me/backend/db/models/eventimage.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class EventImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  EventImage.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    eventId: {
+      type: DataTypes.INTEGER
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    preview: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'EventImage',
+  });
+  return EventImage;
+};
