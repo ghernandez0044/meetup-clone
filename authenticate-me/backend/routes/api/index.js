eaa05bd95db0143e65f2d2d9f33a8e231e3c86d9
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-28 13:03:16 -0800

    finished Get all Groups endpoint

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -2,12 +2,14 @@
 const router = require('express').Router()
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth')
 const sessionRouter = require('./session')
+const groupsRouter = require('./groups')
 
 // Global middleware
 router.use(restoreUser)
 
 // Connecting routers
 router.use('/session', sessionRouter)
+router.use('/groups', groupsRouter)
 
 // Test endpoint
 router.post('/test', (req, res) => {
